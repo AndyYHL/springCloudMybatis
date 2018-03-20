161bb349059186358eebc13aeff23588d88a1421
yhl <love90yhl@hotmail.com>
2018-03-20 11:27:04 +0800

    添加项目拦截器

--- src/main/java/com/hx/eplate/trafficdata/query/interceptor/MyWebAppConfigurer.java
+++ src/main/java/com/hx/eplate/trafficdata/query/interceptor/MyWebAppConfigurer.java
@@ -2,16 +2,16 @@ package com.hx.eplate.trafficdata.query.interceptor;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * Created by Administrator on 2018-03-13.
  */
 @Configuration   //标注此文件为一个配置项，spring boot才会扫描到该配置。该注解类似于之前使用xml进行配置
-public class MyWebAppConfigurer extends WebMvcConfigurerAdapter {
+public class MyWebAppConfigurer implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         //registry.addInterceptor(new ControllerInterceptor()).addPathPatterns("/**");  //对来自/** 这个链接来的请求进行拦截
-        super.addInterceptors(registry);
     }
 }
--- src/main/java/com/hx/eplate/trafficdata/query/util/CorsConfig.java
+++ src/main/java/com/hx/eplate/trafficdata/query/util/CorsConfig.java
@@ -2,13 +2,14 @@ package com.hx.eplate.trafficdata.query.util;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * Created by Administrator on 2018-03-16.
  */
 @Configuration
-public class CorsConfig extends WebMvcConfigurerAdapter {
+public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
